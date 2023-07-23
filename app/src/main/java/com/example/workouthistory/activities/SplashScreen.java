abd9042ad29b1f39977463887dfad4413df4d981
Vlascu <vlascuciulian@gmail.com>
2023-07-23 12:27:49 +0300

    WorkoutHistory

---
+++ app/src/main/java/com/example/workouthistory/activities/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.workouthistory.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.workouthistory.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Register.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
